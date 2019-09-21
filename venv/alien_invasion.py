c3388e22e2ab5aad6cceda06fa978303644ef29c
HyrekanDragon <HyrekanDragon@gmail.com>
2019-09-21 16:48:57 -0700

    Added the ability to fire up to 3 bullets at a time

--- venv/alien_invasion.py
+++ venv/alien_invasion.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-
+from pygame.sprite import Group
 from settings import  Settings
 from ship import  Ship
 import game_functions as gf
@@ -14,11 +14,14 @@ def run_game():
 
     # Make a ship
     ship = Ship(settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Star the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(settings, screen, ship)
+        gf.update_bullet(bullets)
+        gf.update_screen(settings, screen, ship, bullets)
 
 run_game()
--- venv/game_functions.py
+++ venv/game_functions.py
@@ -1,12 +1,15 @@
 import sys
 import  pygame
+from bullet import Bullet
 
-def check_keydown_event(event, ship):
+def check_keydown_event(event, settings, screen, ship, bullets):
     """Respond to keypresses"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
     """Respond to key releases"""
@@ -15,23 +18,41 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship):
+def check_events(settings, screen, ship, bullets):
     """Respond to keypresses and mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            check_keydown_event(event, ship)
-
+            check_keydown_event(event, settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(settings, screen, ship):
+def update_screen(settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen"""
     # Redraw the screen during each pass through the loop.
     screen.fill(settings.get_bg_color())
+    # Redraw al lbulelts behind ship and aliens.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
+
+def update_bullet(bullets):
+    """Update position of bullets and get rid of old bullets"""
+    # Update bullet positions.
+    bullets.update()
+
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullet(settings, screen, ship, bullets):
+    """Fire a bullet if limit not reached yet"""
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < settings.bullets_allowed:
+        new_bullet = Bullet(settings, screen, ship)
+        bullets.add(new_bullet)
