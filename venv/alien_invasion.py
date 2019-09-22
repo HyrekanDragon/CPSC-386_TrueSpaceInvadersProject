b8619a59fd32459d04c66e4ad7f5ff73a3008ddb
HyrekanDragon <HyrekanDragon@gmail.com>
2019-09-22 12:27:56 -0700

    Alien fleet added

--- venv/alien_invasion.py
+++ venv/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.sprite import Group
 from settings import  Settings
 from ship import  Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -12,16 +13,19 @@ def run_game():
     screen = pygame.display.set_mode(settings.get_display())
     pygame.display.set_caption("Alien Invasion")
 
-    # Make a ship
+    # Make a ship, a group of bullets, and a group of aliens
     ship = Ship(settings, screen)
-    # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(settings, screen, ship, aliens)
 
     # Star the main loop for the game.
     while True:
         gf.check_events(settings, screen, ship, bullets)
         ship.update()
         gf.update_bullet(bullets)
-        gf.update_screen(settings, screen, ship, bullets)
+        gf.update_screen(settings, screen, ship, aliens, bullets)
 
 run_game()
